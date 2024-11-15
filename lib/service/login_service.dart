668978c9031bcf827f06140ebe7a3d07db117f79
Carlos Marques <carrlosmr@gmail.com>
2024-11-15 15:26:53 -0300

    feat: adicionando metodo para validar a senha

--- lib/service/login_service.dart
+++ lib/service/login_service.dart
@@ -1,5 +1,6 @@
 import 'package:login_app/data/models/login_model.dart';
 import 'package:login_app/data/sources/api/desafio_flutter_api.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class LoginService {
 
@@ -15,6 +16,20 @@ class LoginService {
     }catch (e){
       return false;
     }
+  }
+
+  String validatePassword(Login login){
+    MultiValidator passwordValidator = MultiValidator([
+      MinLengthValidator(8, errorText: "A senha deve possuir no mínimo 8 caracteres"),
+      PatternValidator(r'[0-9]', errorText: "A senha deve possuir no mínimo 1 número"),
+      PatternValidator(r'[A-Z]', errorText: "A senha deve possuir no mínimo 1 caracter maiúsculo"),
+      PatternValidator(r'[a-z]', errorText: "A senha deve possuir no mínimo 1 caracter maiúsculo"),
+      PatternValidator(r'[!@#$%^&*(),.?":{}|<>]', errorText: "A senha deve possuir no mínimo 1 caracter especial")
+    ]);
+
+    String? errorMessage = passwordValidator(login.password);
+
+    return errorMessage ?? "";
   }
 
 
