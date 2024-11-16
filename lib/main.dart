c765e11fd8944b8252bc4e1f74777022b47ef6f4
Carlos Marques <carrlosmr@gmail.com>
2024-11-16 00:21:12 -0300

    feat: radicionando Load na tela enquanto espera a resposta da requisição e redirecionando para a página home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/view/screens/home_screen.dart';
 import 'package:login_app/view/screens/login_screen.dart';
 
 void main() {
--- lib/view/screens/login_screen.dart
+++ lib/view/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:login_app/core/controllers/login_controller.dart';
 import 'package:login_app/data/models/login_model.dart';
 import 'package:login_app/service/login_service.dart';
+import 'package:login_app/view/screens/home_screen.dart';
 import 'package:login_app/view/widgets/input_login.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,16 +14,21 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   final _formLoginKey = GlobalKey<FormState>();
   final LoginController loginController = LoginController();
   final LoginService loginService = LoginService();
+
+  bool isLoading = false;
 
   Future<bool> sendLogin(Login login) async {
     return await loginService.sendPassword(login);
   }
 
   Future<void> handleLogin() async {
+    setState(() {
+      isLoading = true;
+    });
+
     if (_formLoginKey.currentState!.validate()) {
       Login login = Login(
         name: loginController.nameController.text,
@@ -31,10 +37,12 @@ class _LoginScreenState extends State<LoginScreen> {
 
       bool response = await sendLogin(login);
 
+      setState(() {
+        isLoading = false;
+      });
+
       if (response) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Tudo OK!")),
-        );
+        Navigator.push(context, MaterialPageRoute(builder: (newContext) => HomeScreen()));
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
@@ -52,43 +60,60 @@ class _LoginScreenState extends State<LoginScreen> {
     return Form(
       key: _formLoginKey,
       child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: SingleChildScrollView(
-          child: Center(
-            child: Container(
-              width: 300,
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Login", style: TextStyle(fontSize: 36)),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30, bottom: 20),
-                    child: InputLogin(
-                        labelText: "Nome",
-                        obscureText: false,
-                        inputController: loginController.nameController,
-                        validator: loginController.validateInputName,
-                    ),
+        resizeToAvoidBottomInset: true,
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Center(
+                child: Container(
+                  width: 300,
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Login", style: TextStyle(fontSize: 36)),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, bottom: 20),
+                        child: InputLogin(
+                          labelText: "Nome",
+                          obscureText: false,
+                          inputController: loginController.nameController,
+                          validator: loginController.validateInputName,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: InputLogin(
+                          labelText: "Senha",
+                          obscureText: true,
+                          inputController: loginController.passwordController,
+                          validator: loginController.validateInputPassword,
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                            onPressed: handleLogin,
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green),
+                            child: const Text("Enviar",
+                                style: TextStyle(color: Colors.white))),
+                      )
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 30),
-                    child: InputLogin(
-                        labelText: "Senha",
-                        obscureText: true,
-                        inputController: loginController.passwordController,
-                        validator: loginController.validateInputPassword,
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    child: ElevatedButton(onPressed: handleLogin, child: Text("Enviar", style: TextStyle(color: Colors.white)),
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.green)),
-                  )
-                ],
+                ),
               ),
             ),
-          ),
+            if (isLoading)
+              const Positioned.fill(
+                  child: ColoredBox(
+                color: Colors.black54,
+                child: Center(
+                  child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
+                ),
+              ))
+          ],
         ),
       ),
     );
